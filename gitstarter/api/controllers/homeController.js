77cec378d098a7a7faa3586278dd14a2900bbc5f
Ryuji Mano <rmano@pal-nat186-120-97.itap.purdue.edu>
2018-04-12 07:39:25 -0400

    Merge html with controller

--- gitstarter/api/controllers/homeController.js
+++ gitstarter/api/controllers/homeController.js
@@ -4,13 +4,20 @@ var scraper = new Trending.Scraper();
 exports.getHome = function (req, res, next) {
   req.vueOptions = {
     head: {
-      title: "GitStarter"
+        title: 'GitStarter',
+        scripts: [
+            { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' },
+        ],
+        styles: [
+            { style: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' }
+        ]
     }
   };
   scraper.scrapeTrendingReposFullInfo('').then(repos => {
     const data = {
-      repos: repos
+      ProjectsLists: repos
     };
+    console.log(repos);
     res.renderVue('homePage.vue', data, req.vueOptions);
   }).catch(err => {
     res.renderVue('homePage.vue', {}, req.vueOptions);
