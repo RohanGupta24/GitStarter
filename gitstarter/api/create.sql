8ff6b7d23866cb0048fcd53d294dbc5c09c4a361
Ryuji Mano <rmano@pal-nat186-113-170.itap.purdue.edu>
2018-04-23 02:46:44 -0400

    Fix schema and fix investProject

--- gitstarter/api/create.sql
+++ gitstarter/api/create.sql
@@ -11,17 +11,16 @@ create table Project (
 
 create table Investment (
   investment_id integer primary key,
-  timestamp integer,
   value_bought real,
   value real,
   project_id integer not null references Project(project_id),
   username text not null references Investor(username)
 );
 
-create table Invested (
-  invested_id integer primary key,
-  value_bought real,
-  value_sold real,
+create table Activity (
+  activity_id integer primary key,
+  new_value real,
+  previous_value real,
   timestamp integer,
   project_id integer not null references Project(project_id),
   username text not null references Investor(username)
